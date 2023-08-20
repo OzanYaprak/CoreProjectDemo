e8b906b4177f99b20b6e15dd9bcf1c84d52b27b4
OzanYaprak <ozanerhanyaprak@icloud.com>
2023-08-21 01:18:51 +0300

    Update 21.08

---
+++ CoreProjectDemo/Controllers/LoginController.cs
@@ -0,0 +1,43 @@
+﻿using CoreProjectDemo.Models;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CoreProjectDemo.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public LoginController(SignInManager<AppUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(UserLoginViewModel model)
+        {
+            /* 
+             * Access Fail Count Default Değeri 5'dir.
+             * Default Locklanma Süresi 5 Dk. 
+             */
+
+            if (ModelState.IsValid)
+            {
+                var loginResult = await _signInManager.PasswordSignInAsync(model.Username, model.Password, true, true);
+
+                if (loginResult.Succeeded) { return RedirectToAction("Index", "Home"); }
+                else { ModelState.AddModelError("", "Kullanıcı adı veya şifre yanlış"); }
+            }
+
+            return View();
+        }
+    }
+}
---
+++ CoreProjectDemo/Controllers/LogoutController.cs
@@ -0,0 +1,25 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CoreProjectDemo.Controllers
+{
+    public class LogoutController : Controller
+    {
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public LogoutController(SignInManager<AppUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index()
+        {
+            await _signInManager.SignOutAsync();
+
+            return View();
+        }
+    }
+}
